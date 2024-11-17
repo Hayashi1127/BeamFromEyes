11ca297ebec837e97d1c791f8f5b2798eda38c6b
hys1127 <moyashiis30yen@gmail.com>
2024-11-18 04:05:17 +0900

    コントローラ―系スクリプト作成

---
+++ src/Controller/IReceiver.hpp
@@ -0,0 +1,15 @@
+﻿# pragma once
+
+# include <Siv3D.hpp>
+
+namespace BeamForEyes::Controller
+{
+	class IReceiver
+	{
+	private:
+		Vec3 location;
+	public:
+		virtual ~IReceiver();
+		virtual void GetLocation();
+	};
+}
