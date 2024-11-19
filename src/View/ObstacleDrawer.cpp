a2e6a95b86cdffb190101e952413a83f6aa702b1
hys1127 <moyashiis30yen@gmail.com>
2024-11-20 05:04:20 +0900

    描画処理追加

---
+++ src/View/ObstacleDrawer.cpp
@@ -0,0 +1,32 @@
+﻿# pragma once
+
+# include <Siv3D.hpp>
+
+# include <Model.hpp>
+
+# include "./Model/Obstacle.hpp"
+# include "./View/ObstacleDrawer.hpp"
+
+using namespace BeamForEyes::Model;
+
+namespace BeamForEyes::View
+{
+	void ObstacleDrawer::Draw()
+	{
+		for (int i = 0; i < obstaclePtr->size(); i++)
+		{
+			modelDatabasePtr->obstacleModel.draw(obstaclePtr->at(i).GetPosition(), obstaclePtr->at(i).GetRotation());
+		}
+	}
+
+	ObstacleDrawer::ObstacleDrawer(const Array<Obstacle>* ptr, const ModelDatabase* ptr2)
+	{
+		obstaclePtr = ptr;
+		modelDatabasePtr = ptr2;
+	}
+
+	ObstacleDrawer::~ObstacleDrawer()
+	{
+
+	}
+}
---
+++ src/View/ObstacleDrawer.hpp
@@ -0,0 +1,24 @@
+﻿# pragma once
+
+# include <Siv3D.hpp>
+
+# include <Model.hpp>
+
+# include "./Model/Obstacle.hpp"
+
+using namespace BeamForEyes::Model;
+
+namespace BeamForEyes::View
+{
+	class ObstacleDrawer
+	{
+	private:
+		const Array<Obstacle>* obstaclePtr;
+		const ModelDatabase* modelDatabasePtr;
+
+	public:
+		ObstacleDrawer(const Array<Obstacle>* ptr, const ModelDatabase* ptr2);
+		~ObstacleDrawer();
+		void Draw();
+	};
+}
