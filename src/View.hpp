6002d0a60420bddc6ef5306149fac4c937a87f54
hys1127 <moyashiis30yen@gmail.com>
2024-11-19 20:10:39 +0900

    PostProcess側で定義するので削除

--- src/View.hpp
+++ src/View.hpp
@@ -29,14 +29,6 @@ namespace BeamForEyes::View
 
 		BasicCamera3D mainCamera{ windowSize, 30_deg, Vec3{ 0, 0, -10} };
 
-
-		const PixelShader psBright = HLSL{ U"example/shader/hlsl/extract_bright_linear.hlsl", U"PS" }
-		| GLSL{ U"example/shader/glsl/extract_bright_linear.frag", {{U"PSConstants2D", 0}} };
-		const RenderTexture gaussianA4{ Size{ 1280, 720 } / 4 };
-		const RenderTexture gaussianB4{ Size{ 1280, 720 } / 4 };
-		const RenderTexture gaussianA8{ Size{ 1280, 720 } / 8 };
-		const RenderTexture gaussianB8{ Size{ 1280, 720 } / 8 };
-
 	public:
 		ViewManager(const ModelManager* model, const ControllerManager* controller);
 		~ViewManager();
