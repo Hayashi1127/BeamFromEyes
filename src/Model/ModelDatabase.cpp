111e928d400573c02908d3cd3472de46322b38bb
hys1127 <moyashiis30yen@gmail.com>
2024-11-19 23:52:11 +0900

    モデルデータ管理用クラスを作成

---
+++ src/Model/ModelDatabase.cpp
@@ -0,0 +1,18 @@
+﻿# pragma once
+
+# include "./Model/ModelDatabase.hpp"
+
+const s3d::Model obstacleModel{ U"./ThirdParty/Quaternius_UltimateSpaceKit/Environment/OBJ/Planet_4.obj" };
+
+namespace BeamForEyes::Model
+{
+	ModelDatabase::ModelDatabase()
+	{
+		s3d::Model::RegisterDiffuseTextures(obstacleModel, TextureDesc::MippedSRGB);
+	}
+
+	ModelDatabase::~ModelDatabase()
+	{
+
+	}
+}
---
+++ src/Model/ModelDatabase.hpp
@@ -0,0 +1,15 @@
+﻿# pragma once
+
+# include <Siv3D.hpp>
+
+namespace BeamForEyes::Model
+{
+	class ModelDatabase
+	{
+	public:
+		static const s3d::Model obstacleModel;
+
+		ModelDatabase();
+		~ModelDatabase();
+	};
+}
