99010a701ae28c3805ce9a78c3c7c1464014cc27
Hayashi1127 <naoto884hayashi@gmail.com>
2024-12-11 23:45:49 +0900

    ControllerManagerにMousePointerReceiverの生成や更新を実装

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -3,5 +3,23 @@
 
 namespace BeamForEyes::Controller
 {
+	void ControllerManager::Update()
+	{
+		pointerReceiverPtr->Update();
+	}
 
+	const IPointerReceiver* ControllerManager::GetPointerReceiverPtr() const
+	{
+		return pointerReceiverPtr;
+	}
+
+	ControllerManager::ControllerManager()
+	{
+		pointerReceiverPtr = new MousePointerReceiver();
+	}
+
+	ControllerManager::~ControllerManager()
+	{
+		delete(pointerReceiverPtr);
+	}
 }
