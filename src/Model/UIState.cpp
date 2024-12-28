9ed0aa5ecfe498030631dd5346c6d71b51aa45d8
Hayashi1127 <naoto884hayashi@gmail.com>
2024-12-28 13:36:15 +0900

    UIに表示する情報を保持するクラスを追加

---
+++ src/Model/UIState.cpp
@@ -0,0 +1,42 @@
+﻿# pragma once
+# include <Siv3D.hpp>
+
+# include "./Model/UIState.hpp"
+
+namespace BeamFromEyes::Model
+{
+	int32 UIState::GetCurrentHP()
+	{
+		return currentHP;
+	}
+
+	int32 UIState::GetCurrentMoisture()
+	{
+		return currentMoisture;
+	}
+
+	int32 UIState::GetTime()
+	{
+		return time;
+	}
+
+	void UIState::Update()
+	{
+		this->currentHP = playerPtr->GetHP();
+		this->currentMoisture = playerPtr->GetMoisture();
+
+		time--;
+	}
+
+	UIState::UIState(Player* playerPtr)
+	{
+		this->playerPtr = playerPtr;
+		this->currentHP = playerPtr->GetHP();
+		this->currentMoisture = playerPtr->GetMoisture();
+	}
+
+	UIState::~UIState()
+	{
+
+	}
+}
---
+++ src/Model/UIState.hpp
@@ -0,0 +1,30 @@
+﻿#pragma once
+# include <Siv3D.hpp>
+
+#include "./Model/Player.hpp"
+
+namespace BeamFromEyes::Model
+{
+	class UIState
+	{
+	private:
+		const Player* playerPtr;
+
+		/// @brief 現在のHP
+		int32 currentHP;
+
+		/// @brief ゲームの残り時間
+		int32 time{ 300 };
+
+		/// @brief 現在の潤い
+		int32 currentMoisture;
+	public:
+		UIState(Player* playerPtr);
+		~UIState();
+		void Update();
+
+		int32 GetCurrentHP();
+		int32 GetCurrentMoisture();
+		int32 GetTime();
+	};
+}
