effa7a947717ea549cafc00354d392719928a0aa
Hayashi1127 <naoto884hayashi@gmail.com>
2024-12-31 16:17:45 +0900

    CustomShapeProgressBar追加

---
+++ src/Model/CustomShapeProgressBar.cpp
@@ -0,0 +1,67 @@
+﻿# pragma once
+# include <Siv3D.hpp>
+
+# include "./Model/CustomShapeProgressbar.hpp"
+
+namespace BeamFromEyes::Model
+{
+	ColorF CustomShapeProgressBar::getBarColor(double progress) const
+	{
+		ColorF result = m_barColors.front().second;
+
+		for (auto& c : m_barColors)
+		{
+			if (progress < c.first)
+			{
+				result = c.second;
+			}
+			else
+			{
+				break;
+			}
+		}
+
+		return result;
+	}
+
+	const CustomShapeProgressBar& CustomShapeProgressBar::draw(double value, double maxValue) const
+	{
+		const double progress = maxValue ? Math::Saturate(value / maxValue) : 1.0;
+		const RectF innnerRect = m_shrinkedShape.boundingRect();
+		const RectF innnerRectBar(innnerRect.pos, innnerRect.w * progress, innnerRect.h);
+
+		if (m_shearedX == 0.0)
+		{
+			m_shape.draw(m_backgroundColor);
+
+			if (innnerRectBar.w)
+			{
+				const Polygon bar = innnerRectBar.asPolygon();
+
+				if (const auto g = Geometry2D::And(bar, m_shrinkedShape))
+				{
+					g.front().draw(getBarColor(progress));
+				}
+			}
+		}
+		else
+		{
+			const Vec2 center = m_shape.boundingRect().center();
+			Transformer2D tr(Mat3x2::Translate(-center).shearedX(m_shearedX).translated(center));
+
+			m_shape.draw(m_backgroundColor);
+
+			if (innnerRectBar.w)
+			{
+				const Polygon bar = innnerRectBar.asPolygon();
+
+				if (const auto g = Geometry2D::And(bar, m_shrinkedShape))
+				{
+					g.front().draw(getBarColor(progress));
+				}
+			}
+		}
+
+		return *this;
+	}
+}
---
+++ src/Model/CustomShapeProgressbar.hpp
@@ -0,0 +1,46 @@
+﻿#pragma once
+# include <Siv3D.hpp>
+
+namespace BeamFromEyes::Model
+{
+	class CustomShapeProgressBar
+	{
+	private:
+
+		Polygon m_shape;
+
+		Polygon m_shrinkedShape;
+
+		ColorF m_backgroundColor = ColorF(0.25);
+
+		Array<std::pair<double, ColorF>> m_barColors = {
+			{ 1.0, ColorF(0.1, 0.8, 0.2) }
+		};
+
+		double m_shearedX = 0.0;
+
+		ColorF getBarColor(double progress) const;
+	public:
+
+		CustomShapeProgressBar() = default;
+
+		explicit CustomShapeProgressBar(const Polygon& polygon, double shearedX = 0.0)
+			: CustomShapeProgressBar(polygon, ColorF(0.25), { { 1.0, ColorF(0.1, 0.8, 0.2) } }, shearedX) {}
+
+		CustomShapeProgressBar(const Polygon& polygon, const ColorF& backgroundColor, const ColorF& barColor, double shearedX = 0.0)
+			: CustomShapeProgressBar(polygon, backgroundColor, { { 1.0, barColor } }, shearedX) {}
+
+		CustomShapeProgressBar(const Polygon& polygon, const ColorF& backgroundColor, const Array<std::pair<double, ColorF>>& barColors, double shearedX = 0.0)
+			: m_shape(polygon)
+			, m_shrinkedShape(polygon.calculateBuffer(-1))
+			, m_backgroundColor(backgroundColor)
+			, m_barColors(barColors)
+			, m_shearedX(shearedX)
+		{
+			m_barColors.sort_by([](const auto& a, const auto& b) { return a.first > b.first; });
+		}
+
+		// バーを描く
+		const CustomShapeProgressBar& draw(double value, double maxValue) const;
+	};
+}
