ba5c6476c096671ef1f8116faa65feb0f9fd27ab
Hayashi1127 <naoto884hayashi@gmail.com>
2025-01-01 02:03:24 +0900

    円形のプログレスバーを追加

---
+++ src/Model/CircleProgressBar.cpp
@@ -0,0 +1,43 @@
+﻿#pragma once
+# include <Siv3D.hpp>
+
+# include "./Model/CircleProgressBar.hpp"
+
+namespace BeamFromEyes::Model
+{
+	ColorF CircleProgressBar::getBarColor(double progress) const
+	{
+		ColorF result = barColors.front().second;
+
+		for (auto& c : barColors)
+		{
+			if (progress < c.first)
+			{
+				result = c.second;
+			}
+			else
+			{
+				break;
+			}
+		}
+
+		return result;
+	}
+
+	const CircleProgressBar& CircleProgressBar::draw(double value, double maxValue) const
+	{
+		return draw(this->shape, value, maxValue);
+	}
+
+	const CircleProgressBar& CircleProgressBar::draw(const Circle shape, double value, double maxValue) const
+	{
+		const double progress = maxValue ? Math::Saturate(value / maxValue) : 1.0;
+		const Circle innnerCircle = shape;
+		const Circle innnerCircleBar(shape);
+
+		shape.drawArc(Math::ToRadians(360 - angle/2), Math::ToRadians(angle), 0, 7, backgroundColor);
+		innnerCircleBar.drawArc(Math::ToRadians(360 - angle / 2), Math::ToRadians(angle)*progress, 0, 5, getBarColor(progress));
+
+		return *this;
+	}
+}
---
+++ src/Model/CircleProgressBar.hpp
@@ -0,0 +1,46 @@
+﻿#pragma once
+# include <Siv3D.hpp>
+
+namespace BeamFromEyes::Model
+{
+	class CircleProgressBar
+	{
+	private:
+		Circle shape;
+
+		ColorF backgroundColor = ColorF(0.25);
+
+		/// @brief 残量に応じて色を変える
+		Array<std::pair<double, ColorF>> barColors = {
+			{ 1.0, ColorF(0.1, 0.8, 0.2) }
+		};
+
+		/// @brief バーの描画したい角度
+		int32 angle;
+
+		ColorF getBarColor(double progress) const;
+	public:
+
+		CircleProgressBar() = default;
+
+		explicit CircleProgressBar(const Circle& shape, int32 angle)
+			: CircleProgressBar(shape, ColorF(0.75), { { 1.0, ColorF(0.1, 0.8, 0.2) } }, angle) {}
+
+		CircleProgressBar(const Circle& shape, const ColorF& backgroundColor, const ColorF& barColor, int32 angle)
+			: CircleProgressBar(shape, backgroundColor, { { 1.0, barColor } }, angle) {}
+
+		CircleProgressBar(const Circle& shape, const ColorF& backgroundColor, const Array<std::pair<double, ColorF>>& barColors, int32 angle)
+			: shape(shape)
+			, backgroundColor(backgroundColor)
+			, barColors(barColors)
+			, angle(angle)
+		{
+			this->barColors.sort_by([](const auto& a, const auto& b) { return a.first > b.first; });
+		}
+
+		// バーを描く
+		const CircleProgressBar& draw(double value, double maxValue) const;
+
+		const CircleProgressBar& draw(const Circle shape, double value, double maxValue) const;
+	};
+}
