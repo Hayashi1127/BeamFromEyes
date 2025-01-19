b9fb6130ef9b1417508f33ad25e3f4d8734356f7
Hayashi1127 <naoto884hayashi@gmail.com>
2025-01-19 09:31:54 +0900

    レシーバークラスからNvARクラスを利用できるように

---
+++ src/Controller/EyetrackingReceiver.cpp
@@ -0,0 +1,74 @@
+﻿# pragma once
+
+# include <thread>
+# include <Siv3D.hpp>
+
+# include "EyetrackingReceiver.hpp"
+# include "ExpressionApp.hpp"
+// # include "nvAR.h"
+
+namespace BeamFromEyes::Controller
+{
+	void EyetrackingReceiver::Update()
+	{
+		windowPos = Window::GetPos();
+		sceneSize = Scene::Size();
+		position = Cursor::ScreenPos(); //- windowPos - sceneSize / 2;
+
+		isPressed = MouseL.pressed();
+
+		NvCV_Status err = app.run();
+		if (err != NVCV_SUCCESS)
+		{
+			ReceiveError(err);
+		}
+		else
+		{
+			if (hasCalibData)
+			{
+
+			}
+			for (int i = 0; i < eyeExpressions.size(); i++)
+			{
+				eyeExpressions.at(i) = app._expressions[i + 12];
+			}
+		}
+
+
+	}
+
+	Vec2 EyetrackingReceiver::GetPointerPosition() const
+	{
+		return position;
+	}
+
+	bool EyetrackingReceiver::GetIsPressed() const
+	{
+		return isPressed;
+	}
+
+	EyetrackingReceiver::EyetrackingReceiver()
+	{
+		windowPos = Window::GetPos();
+
+		// Nvidia AR SDK初期化
+		// app初期化
+		NvCV_Status err = app.init();
+		if (err != NVCV_SUCCESS)
+		{
+			ReceiveError(err);
+		}
+
+		eyeExpressions = Array<float>(8);
+		calibExpressions = Array<float>(8);
+	}
+
+	EyetrackingReceiver::~EyetrackingReceiver()
+	{
+	}
+
+	void EyetrackingReceiver::ReceiveError(NvCV_Status error)
+	{
+		Print << error;
+	}
+}
---
+++ src/Controller/EyetrackingReceiver.hpp
@@ -0,0 +1,54 @@
+﻿# pragma once
+
+
+# include <Siv3D.hpp>
+
+# include "IPointerReceiver.hpp"
+# include "ExpressionApp.hpp"
+# include "nvAR.h"
+// # include "nvAR_defs.h"
+// #include "nvCVOpenCV.h"
+
+namespace BeamFromEyes::Controller
+{
+	class EyetrackingReceiver : public IPointerReceiver
+	{
+	private:
+		/// @brief モニター上のゲームウィンドウの位置
+		Point windowPos;
+
+		/// @brief シーンサイズ
+		Size sceneSize;
+
+		/// @brief ポインターの場所
+		Point position{ 0, 0 };
+
+		/// @brief そのフレーム内でマウスを左クリックいているかどうか
+		bool isPressed{ false };
+
+		bool hasCalibData{ false };
+
+		// Nvidia AR SDKまわり
+		App app;
+
+		/*
+		* eyeLookDown_L
+		* eyeLookDown_R
+		* eyeLookIn_L
+		* eyeLookIn_R
+		* eyeLookOut_L
+		* eyeLookOut_R
+		* eyeLookUp_L
+		* eyeLookUp_R
+		*/ 
+		Array<float> eyeExpressions, calibExpressions;
+
+		void ReceiveError(NvCV_Status error);
+	public:
+		EyetrackingReceiver();
+		~EyetrackingReceiver();
+		void Update();
+		Vec2 GetPointerPosition() const;
+		bool GetIsPressed() const;
+	};
+}
