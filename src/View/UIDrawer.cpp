0359a62b64c1dc1c4889453af6b310c1cd34357e
Hayashi1127 <naoto884hayashi@gmail.com>
2025-02-16 19:04:57 +0900

    プレイヤーのHPが0になったら文字を表示する

--- src/View/UIDrawer.cpp
+++ src/View/UIDrawer.cpp
@@ -14,7 +14,12 @@ namespace BeamFromEyes::View
 
 		modelDatabasePtr->hpBar.SetSize(windowSizePtr->x/1.5, 40).SetCenter(windowSizePtr->x / 2, 40).draw(uiStatePtr->GetCurrentHP(), uiStatePtr->GetInitialHP());
 		modelDatabasePtr->moistureBar.SetCenter(uiStatePtr->GetPosition()).draw(uiStatePtr->GetCurrentMoisture(), uiStatePtr->GetInitialMoisture());
-		Circle{ uiStatePtr->GetPosition(), 40 }.draw();
+
+		if (uiStatePtr->GetCurrentHP() <= 0)
+		{
+			font(U"守りきれませんでした...来世に期待ですね").drawAt(windowSizePtr->x / 2, windowSizePtr->y / 2, Palette::White);
+		}
+		Circle{ uiStatePtr->GetPosition(), 10 }.draw();
 	}
 
 	UIDrawer::UIDrawer(const UIState* uiStatePtr, const ModelDatabase* modelDatabasePtr, const Size* sizePtr)
